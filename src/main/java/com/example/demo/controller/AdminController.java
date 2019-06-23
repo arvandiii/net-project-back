96fc0f1e1579ea19e6595ee407103df5aa9bc548
Melikabesharati <melika.besharati76@gmail.com>
2019-06-23 15:14:22 +0430

    admin verify

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -1,4 +1,24 @@
 package com.example.demo.controller;
 
+import com.example.demo.entities.CaseEntity;
+import com.example.demo.entities.UserEntity;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.utils.ResponseWithData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class AdminController {
+    private UserRepository userRepository;
+
+    @Autowired
+    public AdminController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public ResponseWithData<CaseEntity> verifyUser(String username) {
+        UserEntity userEntity = UserRepository.findByUsername(username);
+        userEntity.setVerified(true);
+        return new ResponseWithData<>(true, "case created", userRepository.save(userEntity));
+    }
 }
---
+++ src/main/java/com/example/demo/services/AdminService.java
@@ -0,0 +1,43 @@
+package com.example.demo.services;
+
+import com.example.demo.controller.CaseController;
+import com.example.demo.controller.UserController;
+import com.example.demo.entities.CaseEntity;
+import com.example.demo.entities.UserEntity;
+import com.example.demo.utils.ResponseWithData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Component
+@Path("/admin")
+public class AdminService {
+    private AdminController adminController;
+    
+    @Autowired
+    public AdminService(AdminController adminController) {
+        this.adminController = adminController;
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/verifyUser")
+    public Response verifyUser(@HeaderParam("Authorization") String token, String username) {
+        if (token == null) {
+            return new ResponseWithData<CaseEntity>(false, "no token provided", null).buildResponse();
+        }
+        UserEntity userEntity = userController.getUserByToken(token);
+        if (userEntity == null) {
+            return new ResponseWithData<CaseEntity>(false, "invalid token", null).buildResponse();
+        }
+        if (userEntity.getRole() != Role.MANAGER || !userEntity.isVerified()) {
+            return new ResponseWithData<CaseEntity>(false, "unauthorized", null).buildResponse();
+        }
+        
+        return adminController.verifyUser(username).buildResponse();
+    }
+}
